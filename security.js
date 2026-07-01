df587cff5a4f83760d157f724d56f5d2171f7af8
yefersithopro7143 <74909395+yefersithopro7143@users.noreply.github.com>
2026-06-30 20:17:31 -0500

    Security Updated: ON (MAX PROTECT)

--- security.js
+++ security.js
@@ -1,5 +1,5 @@
 // security.js - Premium Store Security Core (VIP GOD-TIER V4 - ANIQUILACIÓN)
-const SECURITY_ENABLED = false; 
+const SECURITY_ENABLED = true; 
 
 (function() {
   if (!SECURITY_ENABLED) {
